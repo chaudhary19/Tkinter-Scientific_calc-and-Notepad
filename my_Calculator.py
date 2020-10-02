c522aa76763a2b7387177b131334e95b428af2ee
Shreyansh Shukla <rshreyansh532@gmail.com>
2020-10-02 09:29:22 +0530

    Updated calci ui & added few scientific operations

--- my_Calculator.py
+++ my_Calculator.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter.messagebox as tmsg
 import os
 import time
-from math import sin,cos,tan,log
+from math import *
 
 
 def getvals(event):
@@ -46,8 +46,7 @@ root.geometry(f'{canvas_width}x{canvas_height}')
 root.maxsize(canvas_width,canvas_height)
 root.minsize(canvas_width,canvas_height)
 root.title('CalCulator @ Chaudhary_19')
-root.wm_iconbitmap('calculator.png')
-
+root.call('wm', 'iconphoto', root._w, PhotoImage(file='calculator.png'))
 
 
 
@@ -61,46 +60,23 @@ my_menu.add_command(label='Exit',command=quit)
 
 
 sc_variable=StringVar()
-screen=Entry(root,textvariable=sc_variable,font='lucida 35 bold',fg='yellow',bg='black')
-screen.pack(pady=10)
+screen=Entry(root,textvariable=sc_variable,font='lucida 35 bold',fg='black',bg='white',borderwidth=10)
+screen.pack(pady=30)
 
 
-f=Frame(root,padx=20,pady=20)
+f=Frame(root)
 f.pack()
-b1=Button(f,text='7',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b1.pack(side=LEFT,padx=5)
-b2=Button(f,text='8',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b2.pack(side=LEFT,padx=5)
-b3=Button(f,text='9',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b3.pack(side=LEFT,padx=5)
-b4=Button(f,text='*',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b4.pack(side=LEFT,padx=5)
-b5=Button(f,text='sin',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b5.pack(side=LEFT,padx=5)
-b6=Button(f,text='(',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b6.pack(side=LEFT,padx=5)
-b1.bind('<Button-1>',getvals)
-b2.bind('<Button-1>',getvals)
-b3.bind('<Button-1>',getvals)
-b4.bind('<Button-1>',getvals)
-b5.bind('<Button-1>',getvals)
-b6.bind('<Button-1>',getvals)
+b1=Button(f,text='7',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
 
+b2=Button(f,text='8',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
 
-f=Frame(root,padx=20,pady=20)
-f.pack()
-b1=Button(f,text='4',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b1.pack(side=LEFT,padx=5)
-b2=Button(f,text='5',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b2.pack(side=LEFT,padx=5)
-b3=Button(f,text='6',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b3.pack(side=LEFT,padx=5)
-b4=Button(f,text='-',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b4.pack(side=LEFT,padx=5)
-b5=Button(f,text='cos',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b5.pack(side=LEFT,padx=5)
-b6=Button(f,text=')',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b6.pack(side=LEFT,padx=5)
+b3=Button(f,text='9',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='*',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='sin',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='(',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
 
 b1.bind('<Button-1>',getvals)
 b2.bind('<Button-1>',getvals)
@@ -108,21 +84,27 @@ b3.bind('<Button-1>',getvals)
 b4.bind('<Button-1>',getvals)
 b5.bind('<Button-1>',getvals)
 b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+        buttons[count].grid(row=1,column=i)
+        count += 1
 
-f=Frame(root,padx=20,pady=20)
+
+f=Frame(root)
 f.pack()
-b1=Button(f,text='1',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b1.pack(side=LEFT,padx=5)
-b2=Button(f,text='2',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b2.pack(side=LEFT,padx=5)
-b3=Button(f,text='3',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b3.pack(side=LEFT,padx=5)
-b4=Button(f,text='+',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b4.pack(side=LEFT,padx=5)
-b5=Button(f,text='tan',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b5.pack(side=LEFT,padx=5)
-b6=Button(f,text='%',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b6.pack(side=LEFT,padx=5)
+b1=Button(f,text='4',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='5',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='6',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='-',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='cos',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text=')',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
 
 b1.bind('<Button-1>',getvals)
 b2.bind('<Button-1>',getvals)
@@ -130,21 +112,25 @@ b3.bind('<Button-1>',getvals)
 b4.bind('<Button-1>',getvals)
 b5.bind('<Button-1>',getvals)
 b6.bind('<Button-1>',getvals)
-
-f=Frame(root,padx=20,pady=20)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=2,column=i)
+    count += 1
+f=Frame(root)
 f.pack()
-b1=Button(f,text='.',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b1.pack(side=LEFT,padx=5)
-b2=Button(f,text='0',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b2.pack(side=LEFT,padx=5)
-b3=Button(f,text='=',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b3.pack(side=LEFT,padx=5)
-b4=Button(f,text='/',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b4.pack(side=LEFT,padx=5)
-b5=Button(f,text='Clr',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b5.pack(side=LEFT,padx=5)
-b6=Button(f,text='log',font='lucida 30 bold',padx=5,borderwidth=3,fg='yellow',bg='black')
-b6.pack(side=LEFT,padx=5)
+b1=Button(f,text='1',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='2',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='3',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='+',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='tan',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='%',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
 
 b1.bind('<Button-1>',getvals)
 b2.bind('<Button-1>',getvals)
@@ -152,9 +138,65 @@ b3.bind('<Button-1>',getvals)
 b4.bind('<Button-1>',getvals)
 b5.bind('<Button-1>',getvals)
 b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=3,column=i)
+    count += 1
+f=Frame(root)
+f.pack()
+b1=Button(f,text='.',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
 
+b2=Button(f,text='0',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='sinh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='cosh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='tanh',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='pi',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=4,column=i)
+    count += 1
+f=Frame(root)
+f.pack()
+
+b1=Button(f,text='log10',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b2=Button(f,text='exp',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b3=Button(f,text='/',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b4=Button(f,text='Clr',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b5=Button(f,text='log',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+b6=Button(f,text='=',font='lucida 15 bold',padx=20,pady=20,borderwidth=3,fg='black',bg='grey',width=3)
+
+
+b1.bind('<Button-1>',getvals)
+b2.bind('<Button-1>',getvals)
+b3.bind('<Button-1>',getvals)
+b4.bind('<Button-1>',getvals)
+b5.bind('<Button-1>',getvals)
+b6.bind('<Button-1>',getvals)
+buttons=[b1,b2,b3,b4,b5,b6]
+count=0
+for i in range(6):
+    buttons[count].grid(row=5,column=i)
+    count += 1
 status_var=StringVar()
 status_var.set('Ready..')
-Label(root,textvariable=status_var,relief=SUNKEN,anchor='w',borderwidth=2,bg='yellow',fg='red').pack(side=BOTTOM,fill=X)
+Label(root,textvariable=status_var,relief=SUNKEN,anchor='w',borderwidth=3,bg='yellow',fg='red').pack(side=BOTTOM,fill=X)
 
 root.mainloop()
